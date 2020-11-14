d1d33a02c580e896a61411d2ca894d03b99be761
Kuatett <alberttk2009@gmail.com>
2020-11-14 21:04:46 +0000

    intial commit

---
+++ Connected/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter your name");
+
+        Scanner s = new Scanner(System.in);
+        String Al = s.nextLine();
+
+      if (Al.equals("Albert")){
+          System.out.println("Hey Albert");
+      }
+      else if (Al.equals("Sally")){
+          System.out.println("Hey Sally");
+      }
+      else{
+          System.out.println("You're not welcome here");
+      }
+        s.close();
+    }
+}
